b5c73bcf43f6ca10fba4c91de972aa40540dc836
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-06-18 23:28:17 +0600

    created add task page

--- src/pages/TodoHome/Task/AddTask/AddTask.jsx
+++ src/pages/TodoHome/Task/AddTask/AddTask.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import AddTaskStageNav from "./AddTaskStageNav";
+import AddTask_Advance from "./AddTask_Advance";
+import AddTask_Basic from "./AddTask_Basic";
+
+import AddTaskStage from "./AddTaskStage";
+
 const AddTask = () => {
-  return <div>AddTask</div>;
+  const [isAdvance, setIsAdvance] = useState(false);
+  return (
+    <>
+      <div className="bg-Light w-full h-full">
+        <div className=" flex w-full h-full">
+          <AddTaskStageNav setIsAdvance={setIsAdvance} />
+
+          {/* <TaskOrModifieTaskDetils_Basic /> */}
+          <div className="w-full">
+            <AddTaskStage isAdvance={isAdvance} />
+            {isAdvance ? (
+              <AddTask_Advance setIsAdvance={setIsAdvance} />
+            ) : (
+              <AddTask_Basic setIsAdvance={setIsAdvance} />
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AddTask;

b263509e9edf1ca83c1a59736db7849500026105
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-09-15 03:42:24 +0600

    Removed all  temporary file and folders

--- src/Templates/TodoHome/Home/TodoHomeSidebar_temp.jsx
+++
@@ -1,9 +0,0 @@
-const TodoHomeSidebar_temp = () => {
-  return (
-    <div className="bg-Shades md:block hidden h-screen">
-      TodoHomeSidebar_temp
-    </div>
-  );
-};
-
-export default TodoHomeSidebar_temp;

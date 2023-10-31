614b2d55047ed9ac53c839b34cd1f2e1d13cc834
yeasin-2002 <mdkawsarislam2002@gmail.com>
2023-10-31 17:42:42 +0600

    updated path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig(async () => ({
@@ -13,10 +13,19 @@ export default defineConfig(async () => ({
   envPrefix: ["VITE_"],
   resolve: {
     alias: {
-      "@/": path.resolve(__dirname, "src"),
-      "@redux": path.resolve(__dirname, "src/Redux/feature/"),
-      "@page": path.resolve(__dirname, "src/pages/"),
-      "@templates": path.resolve(__dirname, "src/Templates/"),
+      $: path.resolve(__dirname, "./"),
+      $src: path.resolve(__dirname, "./src/"),
+      $assets: `${path.resolve(__dirname, "./src/assets/")}`,
+      $hooks: `${path.resolve(__dirname, "./src/hooks/")}`,
+      $layout: `${path.resolve(__dirname, "./src/layout/")}`,
+      $pages: `${path.resolve(__dirname, "./src/pages/")}`,
+      $utils: `${path.resolve(__dirname, "./src/utils/")}`,
+      $data: `${path.resolve(__dirname, "./src/data")}`,
+      $store: `${path.resolve(__dirname, "./src/store/")}`,
+      $style: `${path.resolve(__dirname, "./src/style/")}`,
+      $components: `${path.resolve(__dirname, "./src/components/")}`,
+      $ui: `${path.resolve(__dirname, "./src/ui/")}`,
+      $lib: `${path.resolve(__dirname, "./src/lib/")}`,
     },
   },
 }));
